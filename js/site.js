d0f6f46d9d0db2ad33db8356690f6f8cebead406
Michelle Joseph <mbergy27@gmail.com>
2021-07-27 13:50:04 -0500

    First Commit
    An invoice template using an onscreen print button

---
+++ js/site.js
@@ -0,0 +1,9 @@
+// const buttonPrint = document.querySelector (
+//     ".button-print"
+// )
+
+// function buttonPrint() {
+//     window.print();
+// }
+
+// buttonPrint.addEventListener("click", buttonPrint)
